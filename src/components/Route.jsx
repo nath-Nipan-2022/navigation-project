c50e98387f08c19bb39eaf9b0afaf12e5c61c0b0
Nipan Debnath <debnathnipan719@gmail.com>
2023-04-22 19:54:50 +0530

    update useNavigaion context

--- src/components/Route.jsx
+++ src/components/Route.jsx
@@ -1,13 +1,12 @@
-import { useContext } from "react";
-import NavigationContext from "../context/navigation";
+import useNavigation from "../hooks/useNavigation";
 
 const Route = ({ path, children }) => {
-  const { currentPath } = useContext(NavigationContext);
+  const { currentPath } = useNavigation();
 
   if (path === currentPath) {
     return children;
   } else {
-    return;
+    return "";
   }
 };
 
