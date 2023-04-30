5db86e968eb983b85cab348cc0e2c9b02e28a904
Nipan Debnath <debnathnipan719@gmail.com>
2023-04-30 09:08:10 +0530

    add modal page

---
+++ src/pages/ModalPage.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import Button from "../components/Button";
+import Modal from "../components/Modal";
+Button;
+
+const ModalPage = () => {
+  const [showModal, setShowModal] = useState(false);
+
+  const closeModal = () => {
+    setShowModal(false);
+  };
+
+  const modal = (
+    <Modal onClose={closeModal} className="bg-blue-100">
+      <h2 className="text-lg ">Hi, I am Modal</h2>
+      <p className="text-gray-600 text-sm mb-1">
+        Lorem ipsum dolor sit amet. Lorem ipsum dolor sit.
+      </p>
+      <Button secondary onClick={closeModal}>
+        Submit
+      </Button>
+    </Modal>
+  );
+
+  return (
+    <div className="p-10">
+      <Button secondary onClick={() => setShowModal(true)} className="mx-auto">
+        Open Modal
+      </Button>
+      {showModal && modal}
+    </div>
+  );
+};
+
+export default ModalPage;
