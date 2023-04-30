1fdcf0211e8cb884bda53cb7fe82c644d5599c77
Nipan Debnath <debnathnipan719@gmail.com>
2023-04-30 09:09:48 +0530

    updated componetns

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,30 @@ import Route from "./components/Route";
 import AccordionPage from "./pages/AccordionPage";
 import DropdownPage from "./pages/DropdownPage";
 import ButtonPage from "./pages/ButtonPage";
+import ModalPage from "./pages/ModalPage";
+import TablePage from "./pages/TablePage";
 
 const App = () => {
   return (
     <div className="">
       <Sidebar />
-
-      <Route path="/">
-        <DropdownPage />
-      </Route>
-      <Route path="/accordion">
-        <AccordionPage />
-      </Route>
-      <Route path="/buttons">
-        <ButtonPage />
-      </Route>
+      <main className=" ml-36 p-4">
+        <Route path="/">
+          <DropdownPage />
+        </Route>
+        <Route path="/accordion">
+          <AccordionPage />
+        </Route>
+        <Route path="/buttons">
+          <ButtonPage />
+        </Route>
+        <Route path="/modal">
+          <ModalPage />
+        </Route>
+        <Route path="/table">
+          <TablePage />
+        </Route>
+      </main>
     </div>
   );
 };
