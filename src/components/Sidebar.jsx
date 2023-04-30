b31cdebd6fceae9726950f637a817fa866c37cc6
Nipan Debnath <debnathnipan719@gmail.com>
2023-04-30 09:09:08 +0530

    Sidebar updated

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,6 +5,8 @@ const Sidebar = () => {
     { label: "Dropdown", path: "/" }, // default page
     { label: "Accordion", path: "/accordion" },
     { label: "Buttons", path: "/buttons" },
+    { label: "Modal", path: "/modal" },
+    { label: "Table", path: "/table" },
   ];
 
   const renderLinks = links.map((link) => {
@@ -12,15 +14,15 @@ const Sidebar = () => {
       <Link
         key={link.label}
         to={link.path}
-        className="p-2 px-3 hover:bg-slate-200 hover:text-blue-600"
-        activeClassName={" font-bold text-blue-600 border-l-2 border-blue-600"}
+        className="p-2 px-3 border-l-2 border-gray-200 hover:text-blue-500 hover:border-blue-600"
+        activeClassName={"font-bold  text-blue-500 border-blue-900"}
       >
         {link.label}
       </Link>
     );
   });
   return (
-    <div className="flex flex-col sticky top-0 left-0 w-36 p-2 bg-slate-100">
+    <div className="flex flex-col fixed top-4 left-0 w-36 p-2">
       {renderLinks}
     </div>
   );
